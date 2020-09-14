9a804c20686f1a845b32a6edd96aa884f0f76aed
ayushman-2008 <69585712+ayushman-2008@users.noreply.github.com>
2020-09-14 21:28:45 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,13 @@
+class rope(body1,body2,offsetX,offsetY){
+    constructor(body1,body2,offsetX,offsetY){
+       this.offsetX=offsetX
+       this.offsetY=offsetY
+       var options={
+           bodyA:body1,
+           bodyB:body2,
+           pointB:{x:this.offsetX,y:offsetY}
+       }
+
+       
+    }
+}
